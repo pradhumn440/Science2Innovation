ec81928c63c0e871d90a7522b5a1f3b84cca25ca
Pradhumn <pradhumnporwal@gmail.com>
2021-05-14 01:45:39 +0530

    Completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,26 +6,26 @@ import "tailwindcss/tailwind.css";
 const Navbar = () => {
     return (
         <div className="md:flex md:flex-shrink flex flex-auto justify-self-center">
-          <div className="flex mx-36 my-1 justify-self-center" style={{width:"72.25rem"}}> 
+          <div className="flex mx-36 my-1 justify-center" style={{width:"72.25rem"}}> 
             <div className="justify-center">
-            <a href=""><img src={Logo} alt="Internfeed" className="h-9 mr-6 my-7"/></a>
+            <a href=""><img src={Logo} alt="Internfeed" className="h-9 mr-6 my-5"/></a>
             </div>
-            <div className="text-base my-9 mx-6 justify-self-center">
+            <div className="text-base my-5 mt-7 mx-6 justify-center">
                 <a href=""><b>Find internship</b></a>
             </div>
-            <div className="text-base my-9 mx-6 justify-self-center">
+            <div className="text-base my-5 mt-7 mx-6 justify-center">
             <a href="">For employers</a>
             </div>
-            <div className="text-base my-9 mx-6 justify-self-center">
+            <div className="text-base my-5 mt-7 mx-6 justify-self-center">
             <a href="">Blog</a>
             </div>
-            <div className="text-base my-9 mr-6 justify-self-center" style={{marginLeft:"16rem"}}>
+            <div className="text-base my-5 mt-7 mr-6 justify-center" style={{marginLeft:"16rem"}}>
             <a href="">Sign up</a>
             </div>
-            <div className="text-base my-9 mx-6 justify-self-center">
+            <div className="text-base my-5 mt-7 mx-6 justify-center">
             <a href="">Sign in</a>
             </div>
-            <button className="px-5 py-2 h-10 my-7 ml-2 bg-black text-white border-black font-bold text-sm justify-self-center">
+            <button className="px-5 py-2 h-9 my-5 ml-2 bg-black text-white border-black font-bold text-sm justify-self-center">
             <a href="">Post internship</a>
             </button>
           </div>
