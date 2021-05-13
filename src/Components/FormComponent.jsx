de04e9fa211f71cd02b3d7e80e63817eaca3b5b0
Pradhumn <pradhumnporwal@gmail.com>
2021-05-14 02:05:30 +0530

    Final

--- src/Components/FormComponent.jsx
+++ src/Components/FormComponent.jsx
@@ -17,13 +17,13 @@ const FormComponent = () => {
                   <b>Keywords</b>
                  </label>
                   <div className="mt-2">
-                      <input class="bg-white appearance-none border-2 border-gray-200 py-2 px-4 mb-6 text-gray-700 leading-tight focus:border-black focus:bg-white  h-12 w-80" id="inline-full-name" type="text" placeholder="Enter Keywords"/>
+                      <input class="bg-white appearance-none border-2 duration-700 border-gray-200 py-2 px-4 mb-6 text-gray-700 leading-tight focus:border-black focus:bg-white hover:border-black h-12 w-80" id="inline-full-name" type="text" placeholder="Enter Keywords"/>
                    </div>
                    <label class="font-medium text-base" for="grid-first-name">
                    <b>Category</b>
                    </label>
                    <div class="relative mt-2">
-                   <select class="block appearance-none bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white  h-12 w-80 mb-6 focus:border-gray-500" id="grid-state">
+                   <select class="block appearance-none bg-white border-2 duration-700 border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white hover:border-black h-12 w-80 mb-6 focus:border-gray-500" id="grid-state">
                         <option>Select Category</option>
                         <option>Engineering</option>
                         <option>Design</option>
@@ -44,12 +44,12 @@ const FormComponent = () => {
                    <b>Location</b>
                    </label>
                    <div class="">
-                      <input class="bg-white appearance-none border-2 border-gray-200 py-2 px-4 text-gray-700 leading-tight focus:border-black focus:bg-white mb-5 mt-2 h-12 w-80" id="inline-full-name" type="text" placeholder="Select City"/>
+                      <input class="bg-white appearance-none border-2 duration-700 border-gray-200 py-2 px-4 text-gray-700 leading-tight focus:border-black hover:border-black focus:bg-white mb-5 mt-2 h-12 w-80" id="inline-full-name" type="text" placeholder="Select City"/>
                    </div>
                 <div>
                    <div class="relative inline-block w-11 mr-2 align-middle select-none transition duration-1000 ease-in">
-                      <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-2 appearance-none border-gray-400"/>
-                    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full cursor-pointer" style={{background:"#A4A4B3"}}></label>
+                      <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-2 duration-700 appearance-none border-gray-400"/>
+                    <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full cursor-pointer bg-gray-400"></label>
                   </div>
                   <label class="text-base justify-items-center pt-2" for="grid-first-name">
                       Remote Friendly
@@ -64,7 +64,7 @@ const FormComponent = () => {
                  Get notified of internships matching your search
                  </label>
                  <div class="mb-5 mt-5 flex items-center">
-                      <input class="bg-white appearance-none border-2 border-gray-200 py-2 px-4 text-gray-700 leading-tight focus:border-black h-12 w-80" id="inline-full-name" type="text" placeholder="Your email"/>
+                      <input class="bg-white appearance-none border-2 duration-700 border-gray-200 py-2 px-4 hover:border-black text-gray-700 leading-tight focus:border-black h-12 w-80" id="inline-full-name" type="text" placeholder="Your email"/>
                    </div>
                   <div className="flex items-center mt-7">
                   <a href=""><span className="bg-black text-white pt-4 pb-4 border-black font-bold text-xs w-80" style={{paddingLeft:"122.5px",paddingRight:"122.5px",marginLeft:"1px"}}>Create Alert</span></a>
